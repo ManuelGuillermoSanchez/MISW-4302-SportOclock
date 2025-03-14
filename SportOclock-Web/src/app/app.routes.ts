4c3ce32a6685d33081604542d73849550fbf7979
Manuel Sanchez <mg.sanchezb1@uniandes.edu.co>
2025-03-14 10:04:24 -0500

    Creación de pagina de crear rutinas

--- SportOclock-Web/src/app/app.routes.ts
+++ SportOclock-Web/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RutinasComponent } from './rutinas/rutinas.component';
+import { CrearRutinaComponent } from './crear-rutina/crear-rutina.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/rutinas', pathMatch: 'full' },
   { path: 'rutinas', component: RutinasComponent },
+  { path: 'crear-rutina', component: CrearRutinaComponent }, 
   { path: 'historial', component: RutinasComponent },
   { path: 'ajustes', component: RutinasComponent }
 ];
---
+++ SportOclock-Web/src/app/crear-rutina/crear-rutina.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-rutina',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-rutina.component.html',
+  styleUrl: './crear-rutina.component.css'
+})
+export class CrearRutinaComponent {
+
+}
